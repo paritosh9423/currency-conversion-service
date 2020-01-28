8cc7f6e37ab0ca6ddf21a7add5c7842578d615b9
ppradeep <paritosh.9423@gmail.com>
2020-01-28 23:37:27 +0530

    setup a communication b/w currency exchange service and conversion service suing RestTemplate

--- src/main/java/com/paritosh/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ src/main/java/com/paritosh/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,10 +1,14 @@
 package com.paritosh.microservices.currencyconversionservice.controller;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.paritosh.microservices.currencyconversionservice.CurrencyConversionBean;
 
@@ -12,7 +16,12 @@ import com.paritosh.microservices.currencyconversionservice.CurrencyConversionBe
 public class CurrencyConversionController {
 	@GetMapping("currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
-		return new CurrencyConversionBean(1000L, from, to, BigDecimal.TEN, quantity, quantity, 8100);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+		ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/{from}/to/{to}", CurrencyConversionBean.class,uriVariables);
+		CurrencyConversionBean response = responseEntity.getBody();
+		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
 	}
 	
 }
