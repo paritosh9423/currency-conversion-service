0f1825b6295cd46d4a57f5bb68318c64e7c5b190
ppradeep <paritosh.9423@gmail.com>
2020-01-29 23:07:18 +0530

    added sleuth support to trace the request

--- src/main/java/com/paritosh/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ src/main/java/com/paritosh/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ import com.paritosh.microservices.currencyconversionservice.CurrencyExchangeServ
 
 @RestController
 public class CurrencyConversionController {
+	private org.slf4j.Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private CurrencyExchangeServicePorxy proxy;
 	
@@ -34,6 +36,7 @@ public class CurrencyConversionController {
 	@GetMapping("currency-converter/feign/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
 		CurrencyConversionBean response  = proxy.retrieveExchangeCalue(from, to);
+		logger.info("{}",response);
 		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
 	}
 }
